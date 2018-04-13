84eb5335104b2fb3b746cbf7ba3fb2afbfc3efa5
brian83199 <brian1999@me.com>
2018-04-12 23:09:22 -0400

    just added the sensor code and fixed some exit code

--- Testing.py
+++ Testing.py
@@ -1,7 +1,6 @@
 import webbrowser
 
 
-
 def menu():
     print("Hello")
     print("Your temperature:      1")
@@ -10,81 +9,99 @@ def menu():
     option1 = eval(input())
     return option1
 
-#Parker Witt has helped me with ordering th functions to allow the program to work properly. Also helped by removing a return option4 that didnt need to be there.
-
+#Parker Witt has helped me with ordering the functions to allow the program to work properly. Also helped by removing a return option4 that didnt need to be there.
 def stress():
     # done = true
     print("Is the stress caused by work, school, or family? ")
     print("[1 = work / 2 = school / 3 = family]: ")
     ans = eval(input())
-    if ans == 1:
+    if (ans < 1) or (ans > 3):
+        print("invalid entry. Please try again")
+        stress()
+    elif ans == 1:
         print("Stress Relief in the Workplace: ")
         print(webbrowser.open('https://www.helpguide.org/articles/stress/stress-in-the-workplace.htm'))
-        done = True
+        exit(Main())
     elif ans == 2:
         print("Stress Relief at School: ")
         print(webbrowser.open('https://www.verywellmind.com/reduce-student-stress-and-excel-in-school-3145175'))
+        exit (main())
     else:
         print("Stress Relief with the Family: ")
         print(webbrowser.open('https://psychcentral.com/lib/tips-to-reduce-family-stress/'))
-        ans = 0
-    exit(main())    
+        exit(main())
+
 
 def anxiety():
     print("Is the temperature caused anxiety? ")
     print("[1 = Yes / 2 = No]: ")
     ans = eval(input())
-    if ans == 1:
+    if (ans < 1) or (ans > 2):
+        print("invalid entry. Please try again")
+        anxiety()
+    elif ans == 1:
         print("Here's how to lower your temperature from anxiety ")
         print(webbrowser.open('https://www.calmclinic.com/anxiety/symptoms/hotness'))
     else:
-        ans = 2
-    exit(main())
+        exit(main())
 
 
 def feeling_sick():
     print("Are you feeling sick? ")
-    print("[1 = Yes / 2 =  No]: ")
+    print("[1 = Yes /2 =  No]: ")
     ans = eval(input())
-    if ans == 1:
-        print("You might want to see a doctor or get some rest and always remember - an apple a day...")
+    if (ans < 1) or (ans > 2):
+        print("invalid entry. Please try again")
+        feeling_sick()
+    elif ans == 1:
+        print("You might want to see a doctor or get some rest")
     else:
-        ans = 2
-    exit(main())
+        exit(main())
 
 
 def naturally_cold_hands():
     print("Do you have naturally cold hands? ")
-    print("[1 = Yes / 2 =  No]: ")
+    print("[1 = Yes / 2 = No]: ")
     ans = eval(input())
-    if ans == 2:
+    if (ans < 1) or (ans > 2):
+        print("invalid entry. Please try again")
+        naturally_cold_hands()
+    elif ans == 2:
         print("Here's how to raise the temperature of your hands: ")
         print(webbrowser.open('http://www.naturalfertilityandwellness.com/raise-basal-body-temperature/'))
     else:
-        ans = 2
-    exit(main())
+        exit(main())
+
 
 def cold_day_outside():
     print("If you were just outside, was it cold out? ")
     print("[1 = Yes / 2 = No]: ")
     ans = eval(input())
-    if ans == 1:
-    	print("Here's how to raise yuor temperature on a cold day: ")
+    if (ans < 1) or (ans > 2):
+        print("invalid entry. Please try again")
+        cold_day_outside()
+    elif ans == 1:
+        print("Here's how to raise your temperature on a cold day: ")
         print(webbrowser.open("https://www.wikihow.com/Increase-Body-Temperature"))
-        ans = 2
-    exit(main())
-
+    else:
+        exit(main())
 
 def workout():
     done = False
     print("Did you just workout? ")
     print("[1 = Yes / 2 = No]: ")
     ans = eval(input())
-    if ans == 1:
+    if (ans<1) or (ans>2):
+        print("invalid entry. Please try again")
+        workout()
+    elif ans == 1:
         print("Here's how to lower your temperature after working out: ")
         print(webbrowser.open("https://healthyliving.azcentral.com/lower-body-temperature-quickly-after-exercise-17366.html"))
-        ans = 2
-    exit(main())
+    else:
+        exit(main())
+
+
+
 
 def questions():
     done = False
@@ -127,13 +144,21 @@ def questions():
 
 
 def temp():
-    print("Enter your temperature: ")
-    temp = float(input())
-    if ((temp < 72.5) or (temp > 80)):
+    tfile = open("/sys/bus/w1/devices/10-000802824e58/w1_slave")
+    text = tfile.read()
+    tfile.close()
+    temperature_data = text.split()[-1]
+    temperature = float(temperature_data[2:])
+    temperature = temperature / 1000
+    temperature = temperature*(9/5)+32
+    print temperature
+    temperature = float(input())
+    if ((temperature < 72.5) or (temperature > 80)):
         print("Are you feeling well today?")
 
     else:
-        done = True
+        print("I am glad you are feeling well")
+        exit(main())
     print('[Yes or No]: ')
     ans = (input())
 
@@ -141,6 +166,7 @@ def temp():
         done = True
     else:
         questions()
+
 
 
 def main():
